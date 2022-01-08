56e20c8ae2e0d42fe7d273250fd21776cd7a04e1
Nate Nichols <nicho997@umn.edu>
2022-01-08 15:39:07 -0600

    whatever

--- mp_gameplay/currency/crafting.lua
+++ mp_gameplay/currency/crafting.lua
@@ -1,5 +1,25 @@
 if minetest.get_modpath("default") then
 	minetest.register_craft({
+		output = "currency:safe",
+		recipe = {
+			{"default:steel_ingot", "default:steel_ingot",
+				"default:steel_ingot"},
+			{"default:steel_ingot", "default:mese_crystal",
+				"default:steel_ingot"},
+			{"default:steel_ingot", "default:steel_ingot",
+				"default:steel_ingot"},
+		}
+	})
+
+	minetest.register_craft({
+		output = "currency:shop",
+		recipe = {
+			{"default:sign_wall"},
+			{"default:chest_locked"},
+		}
+	})
+
+	minetest.register_craft({
 		output = "currency:barter",
 		recipe = {
 			{"default:sign_wall"},
