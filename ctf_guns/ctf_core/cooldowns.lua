1ecf3d9bfb6ff5b5cf82f3d921cc797438aedacb
Nate Nichols <nicho997@umn.edu>
2022-02-14 08:13:23 -0600

    Added a check preventing a cooldown of less than 0.001 seconds

--- ctf_guns/ctf_core/cooldowns.lua
+++ ctf_guns/ctf_core/cooldowns.lua
@@ -1,22 +1,27 @@
 function ctf_core.init_cooldowns()
-	return {
-		players = {},
-		set = function(self, player, time)
-			local pname = PlayerName(player)
+   return {
+      players = {},
+      set = function(self, player, time)
 
-			if self.players[pname] then
-				self.players[pname]:cancel()
+	 if time <= 0.001 then
+	    return
+	 end
+	 
+	 local pname = PlayerName(player)
 
-				if not time then
-					self.players[pname] = nil
-					return
-				end
-			end
+	 if self.players[pname] then
+	    self.players[pname]:cancel()
 
-			self.players[pname] = minetest.after(time, function() self.players[pname] = nil end)
-		end,
-		get = function(self, player)
-			return self.players[PlayerName(player)]
-		end
-	}
+	    if not time then
+	       self.players[pname] = nil
+	       return
+	    end
+	 end
+
+	 self.players[pname] = minetest.after(time, function() self.players[pname] = nil end)
+      end,
+      get = function(self, player)
+	 return self.players[PlayerName(player)]
+      end
+   }
 end
