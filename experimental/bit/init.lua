1621cf5db20f6d7fe633ae74ea1cde7c7c150329
Nate Nichols <nicho997@umn.edu>
2022-02-19 17:17:47 -0600

    Made the bit mod not break when using a new version of minetest

--- experimental/bit/init.lua
+++ experimental/bit/init.lua
@@ -1,1 +1,6 @@
-bit = minetest.request_insecure_environment().require("bit")
+
+
+if bit == nil then
+   local environment = minetest.request_insecure_environment()
+   bit = environment.require("bit")
+end
