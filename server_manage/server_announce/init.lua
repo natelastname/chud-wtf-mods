c64bcc3897ad1cd2df615e91751c7de05503f333
Nate Nichols <nicho997@umn.edu>
2022-03-13 10:29:30 -0500

    Fixed some warnings

--- server_manage/server_announce/init.lua
+++ server_manage/server_announce/init.lua
@@ -6,7 +6,7 @@ local http_api = minetest.request_http_api()
 
 local function on_response(response)
    -- TODO: Set started to true if the response is not 4xx
-   started = true
+   --started = true
    print("Response:")
    print(dump(response))
 end
@@ -18,27 +18,27 @@ local function sendAnnounce(client_names)
    else
       server["action"] = "start"
    end
-   server["port"]         = tonumber(minetest.setting_get("port"))
-   server["address"]      = minetest.setting_get("server_address")
-   server["name"]         = minetest.setting_get("server_name")
-   server["description"]  = minetest.setting_get("server_description")
+   server["port"]         = tonumber(minetest.settings:get("port"))
+   server["address"]      = minetest.settings:get("server_address")
+   server["name"]         = minetest.setting:get("server_name")
+   server["description"]  = minetest.settings:get("server_description")
    server["version"]      = minetest.get_version().string
-   server["url"]          = minetest.setting_get("server_url")
-   server["creative"]     = minetest.setting_getbool("creative_mode")
-   server["damage"]       = minetest.setting_getbool("enable_damage")
-   server["password"]     = minetest.setting_getbool("disallow_empty_password")
-   server["pvp"]          = minetest.setting_getbool("enable_pvp")
+   server["url"]          = minetest:settings:get("server_url")
+   server["creative"]     = minetest.settings:get("creative_mode")
+   server["damage"]       = minetest.settings:get("enable_damage")
+   server["password"]     = minetest.settings:get("disallow_empty_password")
+   server["pvp"]          = minetest.settings:get("enable_pvp")
    server["uptime"]       = minetest.get_server_uptime()
    server["game_time"]    = minetest.get_gametime()
    server["clients"]      = #client_names
    server["clients_max"]  = tonumber(minetest.setting_get("max_users"))
    server["clients_list"] = client_names
    server["gameid"]       = "minetest"
-   server["privs"]        = minetest.setting_get("default_privs")
+   server["privs"]        = minetest.settings:get("default_privs")
    
    local fetch_request = {}
    local json = minetest.write_json(server)
-   fetch_request.url = minetest.setting_get("serverlist_url").."/announce"
+   fetch_request.url = minetest.settings:get("serverlist_url").."/announce"
    fetch_request.post_data = {}
    fetch_request.post_data["json"] = json
    fetch_request.multipart = true
@@ -48,6 +48,12 @@ local function sendAnnounce(client_names)
 end
 
 local function update_serverlist()
+
+   if http_api == nil then
+      minetest.log("error", "Mod server_announce needs to be added to secure.http_mods")
+
+   end
+
    local names = {}
    for _, player in ipairs(minetest.get_connected_players()) do
       local name = player:get_player_name()
