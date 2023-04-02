0c4f7f44289c93c0d9555499d3c7e559e5a15750
Nate Nichols <nathannichols454@gmail.com>
2023-04-02 13:08:59 -0500

    whatever

--- nate_modpack/paintings/init.lua
+++ nate_modpack/paintings/init.lua
@@ -3,9 +3,7 @@
 
 local paintings = {}
 
-
-
-
+minetest.log("LOADING PAINTINGS MOD")
 paintings.dyes = {
 	{"white",      "White"},
 	{"grey",       "Grey"},
@@ -29,30 +27,30 @@ for _, row in ipairs(paintings.dyes) do
 	local desc = row[2]
 	local icon = "painting_icon_outer.png^(painting_icon_inner.png^[colorize:"..desc..":128)"
 	minetest.register_node("paintings:" .. name, {
-		description = desc .. " Painting",
-		drawtype = "nodebox",
-		tiles = {"paintings_" .. name .. ".png"},
-		inventory_image = icon,
-		wield_image = icon,
-		paramtype = "light",
-		paramtype2 = "wallmounted",
-		sunlight_propagates = true,
-		walkable = false,
-		node_box = {
-			type = "wallmounted",
-			wall_top    = {-0.5, 0.4375, -0.5, 0.5, 0.5, 0.5},
-			wall_bottom = {-0.5, -0.5, -0.5, 0.5, -0.4375, 0.5},
-			wall_side   = {-0.5, -0.5, -0.5, -0.4375, 0.5, 0.5},
-		},
-		groups = {snappy = 2, choppy = 2, oddly_breakable_by_hand = 3, flammable = 3},
-		sounds = default.node_sound_wood_defaults(),
+                             description = desc .. " Painting",
+                             drawtype = "nodebox",
+                             tiles = {"paintings_" .. name .. ".png"},
+                             inventory_image = icon,
+                             wield_image = icon,
+                             paramtype = "light",
+                             paramtype2 = "wallmounted",
+                             sunlight_propagates = true,
+                             walkable = false,
+                             node_box = {
+                                type = "wallmounted",
+                                wall_top    = {-0.5, 0.4375, -0.5, 0.5, 0.5, 0.5},
+                                wall_bottom = {-0.5, -0.5, -0.5, 0.5, -0.4375, 0.5},
+                                wall_side   = {-0.5, -0.5, -0.5, -0.4375, 0.5, 0.5},
+                             },
+                             groups = {snappy = 2, choppy = 2, oddly_breakable_by_hand = 3, flammable = 3},
+                             sounds = default.node_sound_wood_defaults(),
 	})
 	minetest.register_craft({
-		output = "paintings:" .. name,
-		recipe = {
-			{"group:stick", "group:stick", "group:stick"},
-			{"group:stick", "wool:" .. name, "group:stick"},
-			{"group:stick", "group:stick", "group:stick"},
-		}
+         output = "paintings:" .. name,
+         recipe = {
+            {"group:stick", "group:stick", "group:stick"},
+            {"group:stick", "wool:" .. name, "group:stick"},
+            {"group:stick", "group:stick", "group:stick"},
+         }
 	})
 end
